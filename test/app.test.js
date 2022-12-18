d8a103240c324ddebf5e55a86e8002669afeaf55
Lecogoni <giraud.nicolas@me.com>
2022-12-18 18:19:23 +0100

    test(MAIN): implement add, sub, multiply, divide test

--- test/app.test.js
+++ test/app.test.js
@@ -36,3 +36,104 @@ describe('A. User input', function() {
     
 });
 
+
+describe('B. Calculate', function() {
+
+    describe('Add',function() {
+        beforeEach(function() {
+            // Initialise l'application avant chaque test
+            app = new Calc();
+        });
+    
+        it ('1. Test the add function between 2 positive numbers', function(done) {
+            expect(app.add(1,2)).to.equal(3);
+            done();
+        });
+    
+        it ('2. Test the add function between 1 positive number and 1 negative number ', function(done) {
+            expect(app.add(3,-2)).to.equal(1);
+            done();
+        });
+    
+        it ('3. Test the add function between 1 positive number and 1 negative number', function(done) {
+            expect(app.add(1,-2)).to.equal(-1);
+            done();
+        });
+    });
+    
+    describe('C. Sub',function() {
+        beforeEach(function() {
+            // Initialise l'application avant chaque test
+            app = new Calc();
+        });
+    
+        it ('1. Test the sub function between 2 positive numbers', function(done) {
+            expect(app.sub(47,32)).to.equal(15);
+            done();
+        });
+    
+        it ('2. Test the sub function between 1 positive number and 1 negative number ', function(done) {
+            expect(app.sub(3,-2)).to.equal(5);
+            done();
+        });
+
+            
+        it ('3. Test the sub function between 1 positive integer and 1 positif float', function(done) {
+            expect(app.sub(7, 6.2)).to.equal(0.8);
+            done();
+        });
+    
+    });
+
+    describe('D. Multiply',function() {
+        beforeEach(function() {
+            // Initialise l'application avant chaque test
+            app = new Calc();
+        });
+    
+        it ('1. Test the multiply function between 2 positive numbers', function(done) {
+            expect(app.multiply(2,12)).to.equal(24);
+            done();
+        });
+    
+        it ('2. Test the multiply function between 1 positive number and 1 negative number ', function(done) {
+            expect(app.multiply(3,-2)).to.equal(-6);
+            done();
+        });
+    
+        it ('3. Test the multiply function between 2 negative numbers', function(done) {
+            expect(app.multiply(-4,-2)).to.equal(8);
+            done();
+        });
+    
+        it ('3. Test the multiply function between 1 positive integer and 1 positif float', function(done) {
+            expect(app.multiply(3, 6.2)).to.equal(18.6);
+            done();
+        });
+    });
+
+    describe('E. Divide',function() {
+        beforeEach(function() {
+            // Initialise l'application avant chaque test
+            app = new Calc();
+        });
+    
+        it ('1. Test the divide function between 2 positive numbers', function(done) {
+            expect(app.divide(6,2)).to.equal(3);
+            done();
+        });
+    
+        it ('2. Test the divide function between 1 positive number and 1  negative number ', function(done) {
+            expect(app.divide(4,-2)).to.equal(-2);
+            done();
+        });
+    
+        it ('3. Test the divide function between 1 positive number and 1 positif float number', function(done) {
+            expect(app.divide(1,0.5)).to.equal(2);
+            done();
+        });
+    });
+
+
+    
+});
